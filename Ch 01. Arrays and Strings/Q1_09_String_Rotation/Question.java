b0ddf34650de7a34fab8ecfe80d03237d76057c8
June Hong <june@skyglobal.com>
2021-03-17 15:18:00 -0700

    Update Question.java

--- Ch 01. Arrays and Strings/Q1_09_String_Rotation/Question.java
+++ Ch 01. Arrays and Strings/Q1_09_String_Rotation/Question.java
@@ -1,4 +1,4 @@
-package Q1_09_String_Rotation;
+// package Q1_09_String_Rotation;
 
 public class Question {
 	public static boolean isSubstring(String big, String small) {
@@ -8,18 +8,18 @@ public class Question {
 			return false;
 		}
 	}
-	
+
 	public static boolean isRotation(String s1, String s2) {
 	    int len = s1.length();
 	    /* check that s1 and s2 are equal length and not empty */
-	    if (len == s2.length() && len > 0) { 
+	    if (len == s2.length() && len > 0) {
 	    	/* concatenate s1 and s1 within new buffer */
 	    	String s1s1 = s1 + s1;
 	    	return isSubstring(s1s1, s2);
 	    }
 	    return false;
 	}
-	
+
 	public static void main(String[] args) {
 		String[][] pairs = {{"apple", "pleap"}, {"waterbottle", "erbottlewat"}, {"camera", "macera"}};
 		for (String[] pair : pairs) {
